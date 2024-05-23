deede83107749f83331f71800021df27b68dcb6e
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2024-05-23 20:31:43 -0300

    Create Slice.js

---
+++ Slice.js
@@ -0,0 +1,5 @@
+var carros = ["Civic", "Elantra", "Focus", "Jetta", "Cruze", "Corolla"];
+var selecionados = carros.slice(2, 4)
+
+var nome = "João José da Silva Xavier";
+var selecionados = nome.slice(4, 9);
